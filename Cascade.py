5cf11820a456515b1ffb6c695b45c3a10b7d1e7e
kerelka <kerelkaa@gmail.com>
2019-04-02 12:41:35 +0700

    Begin training stage 15

--- Cascade.py
+++ Cascade.py
@@ -14,9 +14,9 @@ def dumper(obj):
 
 def cascade_latih(faces_ii_data,non_faces_ii_data,features,level_cascade):
     cascade = []
-    start_stage = 14
+    start_stage = 15
     banned = True
-    path_banned = 'bannen_index_stage13.json'
+    path_banned = 'bannen_index_stage14.json'
     if banned:
         features_stg = []
         banned_index = ul.load_banned_index(path_banned)
--- example.py
+++ example.py
@@ -8,7 +8,7 @@ from Segmentasi import Segmentasi
 from mergerect import mergeRects
 
 #pathGambar
-path = 'test_image_random/image_0045.jpg'
+path = 'test_image_random/photo.jpg'
 
 #load feature and create cascade stage
 features = ul.load_database('database.json')
@@ -32,7 +32,7 @@ gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 
 faces = dt.detect(gray,cascade)
 
-# faces = mergeRects(faces,min_overlap_cnt=4,overlap_rate=0.3)
+faces = mergeRects(faces,min_overlap_cnt=2,overlap_rate=0.3)
 # faces = mergeRects(faces)
 for x,y,w,h in faces:
     cv.rectangle(image,(x,y),(x+w,y+h),(255,255,0),2)
